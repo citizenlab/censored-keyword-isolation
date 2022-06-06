8288181cb6c6d014bc337bbdad9ff66c6979a035
Jeffrey Knockel <jeff@citizenlab.ca>
2022-06-06 10:49:16 -0400

    Add variant of algorithms which preserve order
    Instead of keyword combinations being modeled as an unordered set, these
    variants use tuples.  The advantage of using tuples is that we may
    consistently find keyword combinations where the order of appearance is
    important.  We may also find some combinations where components must
    appear multiple times.  It is also nice just to have a deterministic
    order of representation without having to (e.g.) sort() them.

---
+++ algorithms-ordered.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+def bin_search(S, g, is_censored):
+    """Perform a binary search over g and return the index of the first
+    sensitive component in g.
+    :param S: set of strings to include with test messages
+    :param g: str
+    :return: index of first sensitive character in g
+    """
+    lo, hi = 0, len(g)
+    while hi - lo > 1:
+        mid = (lo + hi) // 2
+        if is_censored(S + (g[mid:],)):
+            lo = mid
+        else:
+            hi = mid
+    return lo
+
+def bisect_right(S, g, before, is_censored):
+    """Perform a binary search over g and return the index of the rightmost
+    censored character instead of the leftmost, prepending 'before' to tested
+    slice.
+    """
+    lo, hi = 0, len(g)
+    while hi - lo > 1:
+        mid = (lo + hi + 1) // 2
+        if not is_censored(S + (before + g[:mid],)):
+            lo = mid
+        else:
+            hi = mid
+    return lo
+
+def comp_aware_bin_split(s, is_censored):
+    """Improved version of bin_split adapted to for identifying keyword combos
+    :return: sensitive keyword combination as a set of strings
+    """
+    C = ()
+    while True:
+        i = bin_search(C, s, is_censored)
+        j = i + 1
+        k = len(s)
+        while j < k:
+            if is_censored(C + (s[i:j], s[i+1:])):
+                k = j
+            else:
+                j = j + 1
+        C = C + (s[i:j],)
+        if j != len(s):
+            s = s[i+1:]
+        else:
+            s = ""
+        if not s or is_censored(C):
+            break
+    return C
+
+def comp_aware_bin_split_2(s, is_censored):
+    """Modified version of comp_aware_bin_split which uses a binary search
+    rather than linear search to identify ends of components.
+    """
+    C = ()
+    while True:
+        i = bin_search(C, s, is_censored)
+        diff = 1
+        j = i + 1
+        k = len(s)
+        while j < k:
+            if is_censored(C + (s[i:j], s[i+1:])):
+                break
+            else:
+                j += diff
+                diff *= 2
+        diff //= 2
+        s_1, j = s[i:], j - i
+        j = i + 1 + bisect_right(C + (s_1[1:],),
+                                 s_1[j-diff:j],
+                                 s_1[:j-diff],
+                                 is_censored)
+        C = C + (s[i:j+diff],)
+        s = s[i+1:]
+        if not s or is_censored(C):
+            break
+    return C
+
+def main():
+    from simulator import Simulator
+    sim = Simulator()
+    def is_censored(test):
+        separator = '\x00' # will be platform specific
+        return sim.send(separator.join(test))
+    for art in sim.get_articles():
+        kw = comp_aware_bin_split(art, is_censored)
+        sim.report_found_keyword(kw)
+        print(sim.query_log[sim.this_article])
+    print(sim.queries / len(sim.articles))
+
+if __name__ == "__main__":
+    main()
