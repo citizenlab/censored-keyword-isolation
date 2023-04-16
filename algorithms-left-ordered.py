1a5e518f7b7d52235828c10e9a618018738b4d0f
Jeffrey Knockel <jeff@citizenlab.ca>
2023-04-16 18:02:51 -0400

    Add "left"-favoring variant of algorithms
    If more than one censored keyword combination is present, these
    algorithms isolate the censored keyword combination whose rightmost
    component is leftmost in the censored text.  The existing algorithms
    isolate the censored keyword combination whose leftmost component is
    rightmost in the censored text.

---
+++ algorithms-left-ordered.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+def bin_search(S, g, is_censored):
+    """Perform a binary search over g and return the index of the rightmost
+    character of the rightmost component of the keyword combination whose
+    rightmost component is leftmost in g.
+    :param S: set of strings to include with test messages
+    :param g: str
+    :return: index of the rightmost character of the rightmost component of the
+    keyword combination whose rightmost component is leftmost in g
+    """
+    lo, hi = 0, len(g)
+    while hi - lo > 1:
+        mid = (lo + hi) // 2
+        if is_censored((g[:mid],) + S):
+            hi = mid
+        else:
+            lo = mid
+    return hi
+
+def bisect_left(S, g, after, is_censored):
+    """Perform a binary search over g and return the index of the leftmost
+    character of the leftmost component of the keyword combination whose
+    leftmost component is rightmost in g.
+    :param S: set of strings to include with test messages
+    :param g: str
+    :param before: append 'after' to tested slices of g
+    :return: index of the leftmost character of the leftmost component of the
+    keyword combination whose leftmost component is rightmost in g
+    """
+    lo, hi = 0, len(g)
+    while hi - lo > 1:
+        mid = (lo + hi) // 2
+        if not is_censored((g[mid:] + after,) + S):
+            hi = mid
+        else:
+            lo = mid
+    return lo
+
+def comp_aware_bin_split(s, is_censored):
+    """Isolates a censored keyword combination.  If more than one censored
+    keyword combination is present, it isolate the one whose rightmost
+    component is leftmost in g.
+    :return: censored keyword combination
+    """
+    C = ()
+    j = len(s)
+    while True:
+        i = bin_search(C, s, is_censored)
+        j = min(i - 1, j - 1)
+        while j > 0:
+            if is_censored((s[:i-1], s[j:i]) + C):
+                break
+            else:
+                j = j - 1
+        C = (s[j:i],) + C
+        if j > 0:
+            s = s[:i-1]
+        else:
+            s = ""
+        if not s or is_censored(C):
+            break
+    return C
+
+def comp_aware_bin_split_2(s, is_censored):
+    """Modified version of comp_aware_bin_split which uses a binary search
+    rather than linear search to identify ends of components.
+    """
+    C = ()
+    j = len(s)
+    while True:
+        i = bin_search(C, s, is_censored)
+        diff = 1
+        j = min(i - 1, j - 1)
+        while j > 0:
+            if is_censored((s[:i-1], s[j:i]) + C):
+                break
+            else:
+                j -= diff
+                diff *= 2
+        diff //= 2
+        s_1 = s[:i]
+        k = max(j, 0)
+        j = k + bisect_left((s_1[:-1],) + C,
+                            s_1[k:j+diff],
+                            s_1[j+diff:],
+                            is_censored)
+        C = (s[j:i],) + C
+        if j > 0:
+            s = s[:i-1]
+        else:
+            s = ""
+        if not s or is_censored(C):
+            break
+    return C
+
+def main():
+    from simulator import Simulator
+    sim = Simulator()
+    def is_censored(test):
+        separator = '\x00' # will be platform specific
+        return sim.send(separator.join(test))
+    for art in sim.get_articles():
+        kw = comp_aware_bin_split(art, is_censored)
+        sim.report_found_keyword(kw)
+        print(sim.query_log[sim.this_article])
+    print(sim.queries / len(sim.articles))
+
+if __name__ == "__main__":
+    main()
---
+++ coroutines-left.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+def bin_search(S, g):
+    lo, hi = 0, len(g)
+    while hi - lo > 1:
+        mid = (lo + hi) // 2
+        was_censored = yield {g[:mid]}.union(S)
+        if was_censored:
+            hi = mid
+        else:
+            lo = mid
+    return hi
+
+def bisect_left(S, g, after):
+    lo, hi = 0, len(g)
+    while hi - lo > 1:
+        mid = (lo + hi) // 2
+        was_censored = yield {g[mid:] + after}.union(S)
+        if not was_censored:
+            hi = mid
+        else:
+            lo = mid
+    return lo
+
+def comp_aware_bin_split(s):
+    C = set()
+    j = len(s)
+    while True:
+        i = yield from bin_search(C, s)
+        j = min(i - 1, j - 1)
+        while j > 0:
+            was_censored = yield {s[:i-1], s[j:i]}.union(C)
+            if was_censored:
+                break
+            else:
+                j = j - 1
+        C = {s[j:i]}.union(C)
+        if j > 0:
+            s = s[:i-1]
+        else:
+            s = ""
+        if not s:
+            break
+        was_censored = yield C
+        if was_censored:
+            break
+    return C
+
+def comp_aware_bin_split_2(s):
+    C = set()
+    j = len(s)
+    while True:
+        i = yield from bin_search(C, s)
+        diff = 1
+        j = min(i - 1, j - 1)
+        while j > 0:
+            was_censored = yield {s[:i-1], s[j:i]}.union(C)
+            if was_censored:
+                break
+            else:
+                j -= diff
+                diff *= 2
+        diff //= 2
+        s_1 = s[:i]
+        k = max(j, 0)
+        j = yield from bisect_left({s_1[:-1]}.union(C),
+                                   s_1[k:j+diff],
+                                   s_1[j+diff:])
+        j += k
+        C = {s[j:i]}.union(C)
+        if j > 0:
+            s = s[:i-1]
+        else:
+            s = ""
+        if not s:
+            break
+        was_censored = yield C
+        if was_censored:
+            break
+    return C
+
+def isolate(isolator, sim):
+    was_censored = None
+    while True:
+        try:
+            test = isolator.send(was_censored)
+        except StopIteration as e:
+            return e.value
+        was_censored = is_censored(test, sim)
+
+def is_censored(test, sim):
+    separator = '\x00' # will be platform specific
+    return sim.send(separator.join(test))
+
+def main():
+    from simulator import Simulator
+    sim = Simulator()
+    for art in sim.get_articles():
+        isolator = comp_aware_bin_split(art)
+        kw = isolate(isolator, sim)
+        sim.report_found_keyword(kw)
+        print(sim.query_log[sim.this_article])
+    print(sim.queries / len(sim.articles))
+
+if __name__ == "__main__":
+    main()
